e5d5ccb6560fc3968fb3d0e32bbcfa682f04862a
mounika <mounika.kodamanchili@otsi.co.in>
2021-09-13 13:33:29 +0530

    First Commit

---
+++ Sample/src/Example1.java
@@ -0,0 +1,15 @@
+
+public class Example1 {
+
+	public void number() {
+		
+	
+		int num=5;
+		System.out.println("Hello");
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(num+"*"+i+"="+num*i);
+		}
+	}
+
+}
